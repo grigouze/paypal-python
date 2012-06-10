7ae20759ff84b4109a0962709062d9059ce5afdb
Grigouze <grigouze@yahoo.fr>
2012-06-10 16:20:21 +0200

    add GetBalance command

--- paypal/interface.py
+++ paypal/interface.py
@@ -23,7 +23,7 @@ else:
     import urllib
 
 logger = logging.getLogger('paypal.interface')
-   
+
 class PayPalInterface(object):
     """
     The end developers will do 95% of their work through this class. API
@@ -34,9 +34,9 @@ class PayPalInterface(object):
         """
         Constructor, which passes all config directives to the config class
         via kwargs. For example:
-        
+
             paypal = PayPalInterface(API_USERNAME='somevalue')
-            
+
         Optionally, you may pass a 'config' kwarg to provide your own
         PayPalConfig object.
         """
@@ -62,7 +62,7 @@ class PayPalInterface(object):
             if isinstance(unencoded_pairs[i], types.UnicodeType):
                 unencoded_pairs[i] = unencoded_pairs[i].encode('utf-8')
         return unencoded_pairs
-    
+
     def _check_required(self, requires, **kwargs):
         """
         Checks kwargs for the values specified in 'requires', which is a tuple
@@ -88,11 +88,11 @@ class PayPalInterface(object):
         """
         Wrapper method for executing all API commands over HTTP. This method is
         further used to implement wrapper methods listed here:
-    
+
         https://www.x.com/docs/DOC-1374
-    
+
         ``method`` must be a supported NVP method listed at the above address.
-    
+
         ``kwargs`` will be a hash of
         """
         # This dict holds the key/value pairs to pass to the PayPal API.
@@ -100,7 +100,7 @@ class PayPalInterface(object):
             'METHOD': method,
             'VERSION': self.config.API_VERSION,
         }
-    
+
         if self.config.API_AUTHENTICATION_MODE == "3TOKEN":
             url_values['USER'] = self.config.API_USERNAME
             url_values['PWD'] = self.config.API_PASSWORD
@@ -111,7 +111,7 @@ class PayPalInterface(object):
         # All values passed to PayPal API must be uppercase.
         for key, value in kwargs.items():
             url_values[key.upper()] = value
-        
+
         # This shows all of the key/val pairs we're sending to PayPal.
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug('PayPal NVP Query Key/Vals:\n%s' % pformat(url_values))
@@ -127,7 +127,7 @@ class PayPalInterface(object):
         response = PayPalResponse(req.text, self.config)
 
         logger.debug('PayPal NVP API Endpoint: %s'% self.config.API_ENDPOINT)
-    
+
         if not response.success:
             logger.error('A PayPal API error was encountered.')
             logger.error('PayPal NVP Query Key/Vals:\n%s' % pformat(url_values))
@@ -139,40 +139,40 @@ class PayPalInterface(object):
 
     def address_verify(self, email, street, zip):
         """Shortcut for the AddressVerify method.
-    
+
         ``email``::
             Email address of a PayPal member to verify.
             Maximum string length: 255 single-byte characters
             Input mask: ?@?.??
         ``street``::
             First line of the billing or shipping postal address to verify.
-    
+
             To pass verification, the value of Street must match the first three
             single-byte characters of a postal address on file for the PayPal member.
-    
+
             Maximum string length: 35 single-byte characters.
             Alphanumeric plus - , . ‘ # \
             Whitespace and case of input value are ignored.
         ``zip``::
             Postal code to verify.
-    
+
             To pass verification, the value of Zip mustmatch the first five
             single-byte characters of the postal code of the verified postal
             address for the verified PayPal member.
-    
+
             Maximumstring length: 16 single-byte characters.
             Whitespace and case of input value are ignored.
         """
         args = self._sanitize_locals(locals())
         return self._call('AddressVerify', **args)
-    
+
     def create_recurring_payments_profile(self, **kwargs):
         """Shortcut for the CreateRecurringPaymentsProfile method.
         Currently, this method only supports the Direct Payment flavor.
-        
+
         It requires standard credit card information and a few additional
         parameters related to the billing. e.g.:
-        
+
             profile_info = {
                 # Credit card information
                 'creditcardtype': 'Visa',
@@ -195,7 +195,7 @@ class PayPalInterface(object):
                 'desc': '6 months of our product.'
             }
             response = create_recurring_payments_profile(**profile_info)
-            
+
             The above NVPs compose the bare-minimum request for creating a
             profile. For the complete list of parameters, visit this URI:
             https://www.x.com/docs/DOC-1168
@@ -204,15 +204,15 @@ class PayPalInterface(object):
 
     def do_authorization(self, transactionid, amt):
         """Shortcut for the DoAuthorization method.
-    
+
         Use the TRANSACTIONID from DoExpressCheckoutPayment for the
         ``transactionid``. The latest version of the API does not support the
         creation of an Order from `DoDirectPayment`.
-    
+
         The `amt` should be the same as passed to `DoExpressCheckoutPayment`.
-    
+
         Flow for a payment involving a `DoAuthorization` call::
-    
+
              1. One or many calls to `SetExpressCheckout` with pertinent order
                 details, returns `TOKEN`
              1. `DoExpressCheckoutPayment` with `TOKEN`, `PAYMENTACTION` set to
@@ -222,17 +222,17 @@ class PayPalInterface(object):
                 amount of the transaction.
              1. `DoCapture` with the `AUTHORIZATIONID` (the `TRANSACTIONID`
                 returned by `DoAuthorization`)
-    
+
         """
         args = self._sanitize_locals(locals())
         return self._call('DoAuthorization', **args)
 
     def do_capture(self, authorizationid, amt, completetype='Complete', **kwargs):
         """Shortcut for the DoCapture method.
-    
+
         Use the TRANSACTIONID from DoAuthorization, DoDirectPayment or
         DoExpressCheckoutPayment for the ``authorizationid``.
-    
+
         The `amt` should be the same as the authorized transaction.
         """
         kwargs.update(self._sanitize_locals(locals()))
@@ -240,11 +240,11 @@ class PayPalInterface(object):
 
     def do_direct_payment(self, paymentaction="Sale", **kwargs):
         """Shortcut for the DoDirectPayment method.
-    
+
         ``paymentaction`` could be 'Authorization' or 'Sale'
-    
+
         To issue a Sale immediately::
-    
+
             charge = {
                 'amt': '10.00',
                 'creditcardtype': 'Visa',
@@ -261,15 +261,15 @@ class PayPalInterface(object):
                 'currencycode': 'USD',
             }
             direct_payment("Sale", **charge)
-    
+
         Or, since "Sale" is the default:
-    
+
             direct_payment(**charge)
-    
+
         To issue an Authorization, simply pass "Authorization" instead of "Sale".
-    
+
         You may also explicitly set ``paymentaction`` as a keyword argument:
-    
+
             ...
             direct_payment(paymentaction="Sale", **charge)
         """
@@ -278,7 +278,7 @@ class PayPalInterface(object):
 
     def do_void(self, **kwargs):
         """Shortcut for the DoVoid method.
-    
+
         Use the TRANSACTIONID from DoAuthorization, DoDirectPayment or
         DoExpressCheckoutPayment for the ``AUTHORIZATIONID``.
 
@@ -296,10 +296,10 @@ class PayPalInterface(object):
         * TOKEN
         """
         return self._call('GetExpressCheckoutDetails', **kwargs)
-        
+
     def get_transaction_details(self, **kwargs):
         """Shortcut for the GetTransactionDetails method.
-    
+
         Use the TRANSACTIONID from DoAuthorization, DoDirectPayment or
         DoExpressCheckoutPayment for the ``transactionid``.
 
@@ -340,10 +340,10 @@ class PayPalInterface(object):
         * PAYMENTACTION
         * PAYERID
         * AMT
-                
+
         """
         return self._call('DoExpressCheckoutPayment', **kwargs)
-        
+
     def generate_express_checkout_redirect_url(self, token):
         """Returns the URL to redirect the user to for the Express checkout.
 
@@ -358,9 +358,9 @@ class PayPalInterface(object):
         """
         url_vars = (self.config.PAYPAL_URL_BASE, token)
         return "%s?cmd=_express-checkout&token=%s" % url_vars
-    
+
     def generate_cart_upload_redirect_url(self, **kwargs):
-        """https://www.sandbox.paypal.com/webscr 
+        """https://www.sandbox.paypal.com/webscr
             ?cmd=_cart
             &upload=1
         """
@@ -375,42 +375,42 @@ class PayPalInterface(object):
             #noinspection PyUnresolvedReferences
             additional = urllib.urlencode(additional)
         return url + "&" + additional
-    
+
     def get_recurring_payments_profile_details(self, profileid):
         """Shortcut for the GetRecurringPaymentsProfile method.
-        
+
         This returns details for a recurring payment plan. The ``profileid`` is
         a value included in the response retrieved by the function
         ``create_recurring_payments_profile``. The profile details include the
         data provided when the profile was created as well as default values
         for ignored fields and some pertinent stastics.
-        
+
         e.g.:
             response = create_recurring_payments_profile(**profile_info)
             profileid = response.PROFILEID
             details = get_recurring_payments_profile(profileid)
-        
+
         The response from PayPal is somewhat self-explanatory, but for a
         description of each field, visit the following URI:
-        https://www.x.com/docs/DOC-1194 
+        https://www.x.com/docs/DOC-1194
         """
         args = self._sanitize_locals(locals())
         return self._call('GetRecurringPaymentsProfileDetails', **args)
-    
+
     def manage_recurring_payments_profile_status(self, profileid, action):
         """Shortcut to the ManageRecurringPaymentsProfileStatus method.
-        
+
         ``profileid`` is the same profile id used for getting profile details.
         ``action`` should be either 'Cancel', 'Suspend', or 'Reactivate'.
         """
         args = self._sanitize_locals(locals())
         return self._call('ManageRecurringPaymentsProfileStatus', **args)
-    
+
     def update_recurring_payments_profile(self, profileid, **kwargs):
         """Shortcut to the UpdateRecurringPaymentsProfile method.
-        
+
         ``profileid`` is the same profile id used for getting profile details.
-        
+
         The keyed arguments are data in the payment profile which you wish to
         change. The profileid does not change. Anything else will take the new
         value. Most of, though not all of, the fields available are shared
@@ -432,3 +432,9 @@ class PayPalInterface(object):
         """
         kwargs.update(self._sanitize_locals(locals()))
         return self._call('BMCreateButton', **kwargs)
+
+    def get_balance(self, **kwargs):
+        """Shortcut to the GetBalance method.
+        """
+        kwargs.update(self._sanitize_locals(locals()))
+        return self._call('GetBalance', **kwargs)
